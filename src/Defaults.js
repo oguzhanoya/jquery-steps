a6f982f9d49f667bc19ea55b9bc67a8726a85c29
oguzhanoya <oguzhanoya@outlook.com>
2017-04-20 02:04:04 +0300

    Initial commit

--- src/Defaults.js
+++
@@ -1,9 +0,0 @@
-export default {
-  startAt: 0,
-  showBackButton: true,
-  showFooterButtons: true,
-  onInit: $.noop,
-  onDestroy: $.noop,
-  onFinish: $.noop,
-  onChange() { return true; },
-};
--- test/spec.js
+++
@@ -1,8 +0,0 @@
-describe('My Plugin', function () {
-
-  it('version', function () {
-    var ver = $.fn.steps.version;
-    expect(ver).toEqual('1.0.0');
-  });
-
-});
